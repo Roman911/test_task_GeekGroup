2bb9941c3d28f36f4d314d97fc8521abce40b96a
Roman911 <roma-lysyk@ukr.net>
2022-10-06 16:54:28 +0300

    Fixed header and filter. Customization bootstrap

--- src/scripts/range.js
+++ src/scripts/range.js
@@ -8,48 +8,55 @@ const lastSign = document.getElementById("last-sign");
 
 const children = firstRange.parentNode.childNodes[1].childNodes;
 
-let value1 = 0;
-let value2 = 100;
+let valueMin = 0;
+let valueMax = 54;
 
-const f1 = (input, sign) => {
+let priceMin = 980;
+let priceMax = 40930;
+
+const thumbMin = (input, sign) => {
   input.value = Math.min(input.value, input.parentNode.childNodes[5].value - 1);
-  children[1].style.width = value1 + "%";
-  children[5].style.left = value1 + "%";
-  children[7].style.left = value1 + "%";
+  children[1].style.width = valueMin + "%";
+  children[5].style.left = valueMin + "%";
+  children[7].style.left = valueMin + "%";
 
-  sign.innerHTML = value1;
+  if (valueMin === 0) {
+    sign.innerHTML = priceMin;
+  } else {
+    sign.innerHTML = Math.round((valueMin * (priceMax - priceMin)) / 100);
+  }
 };
 
-const f2 = (input, sign) => {
+const thumbMax = (input, sign) => {
   input.value = Math.max(
     input.value,
     input.parentNode.childNodes[3].value - -1
   );
-  children[3].style.width = 100 - value2 + "%";
-  children[5].style.right = 100 - value2 + "%";
-  children[9].style.left = value2 + "%";
+  children[3].style.width = 100 - valueMax + "%";
+  children[5].style.right = 100 - valueMax + "%";
+  children[9].style.left = valueMax + "%";
 
-  sign.innerHTML = value2;
-};
-
-const f = (input, sign, func) => {
-  func(input, sign);
+  if (valueMax === 100) {
+    sign.innerHTML = priceMax;
+  } else {
+    sign.innerHTML = Math.round((valueMax * (priceMax - priceMin)) / 100);
+  }
 };
 
 firstRange.addEventListener("mousemove", () => {
-  f(firstRange, firstSign, f1);
-  if (value1 < value2) {
-    value1 = Math.round((firstRange.value / parseInt(firstRange.max)) * 100);
+  thumbMin(firstRange, firstSign);
+  if (valueMin < valueMax) {
+    valueMin = Math.round((firstRange.value / parseInt(firstRange.max)) * 100);
   } else {
-    value1 = value2 - 1;
+    valueMin = valueMax - 1;
   }
 });
 
 lastRange.addEventListener("mousemove", () => {
-  f(lastRange, lastSign, f2);
-  if (value1 < value2) {
-    value2 = Math.round((lastRange.value / parseInt(lastRange.max)) * 100);
+  thumbMax(lastRange, lastSign);
+  if (valueMin < valueMax) {
+    valueMax = Math.round((lastRange.value / parseInt(lastRange.max)) * 100);
   } else {
-    value2 = value1 + 1;
+    valueMax = valueMin + 1;
   }
 });
